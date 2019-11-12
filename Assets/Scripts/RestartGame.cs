0a2612dfa55bfdba8f108b762debdf99541641c5
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2019-11-13 02:32:55 +0530

    First version of game is uploaded

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour {
+
+    [SerializeField]
+    string strTag;
+
+    // Update is called once per frame
+    private void OnCollisionEnter (Collision collision) {
+        if (collision.collider.tag == strTag)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
+
---
+++ Assets/Scripts/nextStage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextStage : MonoBehaviour {
+
+    [SerializeField]
+    string strStage;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == strStage)
+            Debug.Log("colliosion detect");
+            SceneManager.LoadScene("level02");
+    }
+}
